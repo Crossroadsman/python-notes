f015727fa30619d6dd9417e5375f3b4cc01ff1a9
Alex Koumparos <crossroadsman@gmail.com>
2019-01-11 14:06:09 -0700

    Create notes.py

---
+++ frameworks/flask/flask_addons/flask-RESTful/notes.py
@@ -0,0 +1,9 @@
+flask-RESTful
+=============
+
+Installation
+------------
+(we don't need to manually specify flask as it is a dependency of flask-RESTful)
+```console
+(venv) $ pip3 install flask-restful peewee
+```
